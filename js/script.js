47995eb7aec2d81af12b6aff274756cbdaab04d0
Bruno Almeida <brunogxp@gmail.com>
2022-06-03 11:17:00 -0300

    js-final

--- js/script.js
+++ js/script.js
@@ -4,24 +4,54 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
 }
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // Verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-      navigation.classList.add('scroll')
-    } else {
-      navigation.classList.remove('scroll')
-    }
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
 }
-
-
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 500) {
-      backToTopButton.classList.add('show')
-    } else {
-      backToTopButton.classList.remove('show')
-    }
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
 }
 
 function openMenu() {
@@ -36,6 +66,6 @@ function closeMenu() {
 
 ScrollReveal({
   origin: 'top',
-  distance:'30px',
+  distance: '30px',
   duration: 700,
 }).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content')
